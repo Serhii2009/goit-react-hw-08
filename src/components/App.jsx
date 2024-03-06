7baa9c687c9795a8883b0ae6504b955b409b1cdf
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-03-06 14:04:40 +0200

    delete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,60 @@
 import { useEffect, lazy } from "react";
 import { useDispatch } from "react-redux";
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import { Layout } from "./Layout";
-import { PrivateRoute } from "./PrivateRoute";
-import { RestrictedRoute } from "./RestrictedRoute";
 import { refreshUser } from "../redux/auth/operations";
-import { useAuth } from "../hooks";
+import { useAuth } from "../hooks/useAuth";
+import { RestrictedRoute } from "../RestrictedRoute";
 
-const HomePage = lazy(() => import("../pages/Home"));
-const RegisterPage = lazy(() => import("../pages/Register"));
-const LoginPage = lazy(() => import("../pages/Login"));
-const TasksPage = lazy(() => import("../pages/Tasks"));
+import { PrivateRoute } from "../PrivateRoute";
+import { MyLoader } from "../Loader/Loader";
+import { Layout } from "../Layout/Layout";
 
-export const App = () => {
+const Home = lazy(() => import("../../pages/Home/Home"));
+const Register = lazy(() => import("../../pages/Register/Register"));
+const Login = lazy(() => import("../../pages/Login/Login"));
+const ContactPage = lazy(() => import("../../pages/ContactPage/ContactPage"));
+
+function App() {
   const dispatch = useDispatch();
   const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
-
   return isRefreshing ? (
-    <b>Refreshing user...</b>
+    <MyLoader />
   ) : (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
+          <Route index element={<Home />} />
           <Route
             path="/register"
             element={
               <RestrictedRoute
-                redirectTo="/tasks"
-                component={<RegisterPage />}
+                redirectTo="/contacts"
+                component={<Register />}
               />
             }
           />
           <Route
             path="/login"
             element={
-              <RestrictedRoute redirectTo="/tasks" component={<LoginPage />} />
+              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
             }
           />
           <Route
-            path="/tasks"
+            path="/contacts"
             element={
-              <PrivateRoute redirectTo="/login" component={<TasksPage />} />
+              <PrivateRoute redirectTo="/login" component={<ContactPage />} />
             }
           />
         </Route>
       </Routes>
-      <Toaster />
+      {/* <Toaster /> */}
     </>
   );
-};
+}
+
+export default App;
